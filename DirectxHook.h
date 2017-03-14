ba90c4bed759d4935bec448418b70e1670b8b974
= <=>
2017-03-14 20:06:34 -0300

    remove

--- DirectxHook.h
+++
@@ -1,7 +0,0 @@
-#ifndef DIRECTXHOOK_HEADER
-#define DIRECTXHOOK_HEADER
-
-int InitializeHook(void);
-void KeyWall();
-
-#endif
--- MemoryScanner.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMORY_SCANNER_HEADER
-#define MEMORY_SCANNER_HEADER
-
-#include <Windows.h>
-
-void ScanMemmory();
-void StartMemoryScan();
-
-#endif
